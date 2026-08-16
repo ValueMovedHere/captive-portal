73f6ab377f7001af4bc49f06ea7ce7a09ed5b6bd
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-08-16 23:47:13 +0800

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, Responder, get};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("localhost:8080")?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, World"
 }
