fdedb3b387129c8a23ec5dc0b73c3a6629f12f73
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-08-20 22:55:43 +0800

    Remove duplicate cli option

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,6 @@ use clap::Parser;
 pub struct Args {
     #[arg(short, long, default_value_t = 8080u16)]
     pub port: u16,
-    #[arg(short, long, default_value_t = "./pages".to_string())]
-    pub pages_path: String, 
+    #[arg(long, default_value_t = "./pages".to_string())]
+    pub pages_path: String,
 }
