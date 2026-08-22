04433f83b7c3b4ba1996894dff0f01ce27d574b5
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-08-22 20:45:21 +0800

    Allow the use of alternative pages dir

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,14 @@ mod submissions;
 async fn main() -> std::io::Result<()> {
     let args = cli::Args::parse();
     let port = args.port;
+    let pages_dir = args.pages_path;
     let msg = format!("Listening on port {port}");
     println!("{}", msg.bold().bright_green());
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
             .service(handlers::redirect_login)
             .service(submissions::submit)
-            .service(Files::new("/login", "./pages"))
+            .service(Files::new("/login", &pages_dir))
             .default_service(web::to(handlers::not_found))
     })
     .bind(format!("localhost:{port}"))?
