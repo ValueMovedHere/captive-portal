93ce313ea09359a88e2e51856237d49471ba843a
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-08-22 16:18:36 +0800

    Display submitted info

--- src/submissions.rs
+++ src/submissions.rs
@@ -1,4 +1,5 @@
-use actix_web::{HttpResponse, Responder, post, web};
+use actix_web::{HttpResponse, http::header, post, web};
+use colored::Colorize;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -10,7 +11,13 @@ struct InfoForm {
 }
 
 #[post("/auth/login")]
-pub async fn submit(form: web::Form<InfoForm>) -> impl Responder {
-    println!("Name: {}", form.name);
-    HttpResponse::Ok().body("Success")
+pub async fn submit(form: web::Form<InfoForm>) -> HttpResponse {
+    println!("{}", "[!] New submission".blue().bold());
+    print!(
+        "\tName: {}\nPhone: {}\nEmail: {}",
+        form.name, form.phone, form.email
+    );
+    HttpResponse::Found()
+        .insert_header((header::LOCATION, "/success.html"))
+        .finish()
 }
