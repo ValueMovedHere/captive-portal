caedc4927212d3e20ae465a36c4cd070c5a96f51
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-04 13:04:34 +0800

    Start airodump-ng to scan APs and station

---
+++ src/airodumpng_scan.rs
@@ -0,0 +1,14 @@
+use std::path::Path;
+use std::process::Command;
+
+fn wirte_scan_result(path: &Path) {
+    let path = format!("{}/result", path.to_string_lossy());
+    Command::new("airodump-ng")
+        .arg("wlan0mon")
+        .arg("--output-format")
+        .arg("csv")
+        .arg("-w")
+        .arg(path)
+        .output()
+        .expect("Failed to start airodump-ng");
+}
